c672a7a4e6c5352a8e4959ac1f4ec8f74fad955a
NOM1989 <70205169+NOM1989@users.noreply.github.com>
2021-06-30 23:50:42 +0100

    feat: bless detect

--- cogs/events.py
+++ cogs/events.py
@@ -11,16 +11,20 @@ class Events(commands.Cog):
             content = message.content.lower()
             spaces_count = content.count(' ')
             if content == 'hold up':
-                await message.channel.send(':raised_hand::woman_police_officer::raised_back_of_hand:')
-            elif content.find('another day') != -1 and spaces_count <= 3:
+                officer_emojis = (':police_officer:', ':man_police_officer:', ':woman_police_officer:')
+                await message.channel.send(f':raised_hand:{choice(officer_emojis)}:raised_back_of_hand:')
+            if content.find('another day') != -1 and spaces_count <= 3:
                 #Add some kind of cooldown so ppl cant spam it so it seems more sentient
                 another_say_emojis = ('<:arv1:849391638254190622><:arv2:849391640506662912>', ':pensive:', ':ok_hand:', ':weary:', '<:pooooG:774059516328804352>', '<:troll:777839853487783956>', '<:okcool:804285714312986645>')
                 await message.channel.send(f'another say {choice(another_say_emojis)}')
-            elif content.find('another say') != -1 and spaces_count <= 3:
+            if content.find('another say') != -1 and spaces_count <= 3:
                 await message.add_reaction('<:okcool:804285714312986645>')
-            elif message.author.id != 421362214558105611 and content.find('true say') != -1 and spaces_count <= 6:
+            if message.author.id != 421362214558105611 and content.find('true say') != -1 and spaces_count <= 6:
                 no_emojis = ('😤', '😮‍💨', '😡', '🤬', '🤫', '🤔', '🙄', '😵', '🤮', '🥸', '🤡', '👺', '🙅', '🙅‍♀️', '🙅‍♂️', '⚔️', '❤️‍🔥', '❌', '💢', '❗', '‼️', '⁉️', '⚠️', '✖️', '🃏')
                 await message.add_reaction(choice(no_emojis))
+            if content.find('bless up') != -1 and spaces_count <= 3:
+                bless_emojis = ('🙏', '😬')
+                await message.add_reaction(choice(bless_emojis))
 
 def setup(bot):
     bot.add_cog(Events(bot))
