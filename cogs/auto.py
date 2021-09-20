f6ae66b479a0169b709533478c5eeeafd9b9e860
NOM1989 <70205169+NOM1989@users.noreply.github.com>
2021-09-20 23:14:09 +0100

    fix: handles bulk now

--- cogs/auto.py
+++ cogs/auto.py
@@ -18,17 +18,27 @@ class Auto(commands.Cog):
     #         # spaces_count = content.count(' ')
     #         # matched_int_ranges = re.search(r"(?:^|[^\d])(\d{1,3}-\d{1,3})(?:[^\d]|$)|(?:[^\d-](\d{1,3}(?:,[\d]{1,3})+))") #Maths teacher shenanegans
             
-    @commands.Cog.listener()
-    async def on_message_delete(self, message):
+    async def _on_delete_handler(self, message):
         if message.id in self.callouts:
-            await message.channel.send(self.callouts[message.id], allowed_mentions=no_mentions)
+            sent = await message.channel.send(self.callouts[message.id], allowed_mentions=no_mentions)
+            self.callouts[sent.id] = str(self.callouts[message.id])
+            del self.callouts[message.id]
         elif message.guild and message.guild.id == rog_server_id and not message.author.bot:
             content = message.content.lower()
-            possible_role_mentions = await re.search(r"<@&\d{18}>", content)
+            possible_role_mentions = re.findall(r"<@&\d{18}>", content)
             if possible_role_mentions:
                 string_to_send = f"{message.author.mention} mentioned: {', '.join(possible_role_mentions)}"
                 sent = await message.channel.send(string_to_send, allowed_mentions=no_mentions)
                 self.callouts[sent.id] = string_to_send
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        await self._on_delete_handler(message)
+
+    @commands.Cog.listener()
+    async def on_bulk_message_delete(self, messages):
+        for message in messages:
+            await self._on_delete_handler(message)
 
 def setup(bot):
     bot.add_cog(Auto(bot))
