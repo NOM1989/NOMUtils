56273a6a660eda919b3468a666129eb5fed1df35
NOM1989 <70205169+NOM1989@users.noreply.github.com>
2022-02-01 23:57:42 +0000

    update: Added Bot class

--- bot.py
+++ bot.py
@@ -8,26 +8,46 @@ import sys
 # from asyncio import sleep
 import asyncpg
 
-extensions = ('cogs.owner', 'cogs.error_handler', 'cogs.admin', 'cogs.tools', 'cogs.starboard', 'cogs.poofboard', 'cogs.summary', 'cogs.auto', 'cogs.minecraft') #cogs.responses
-# extensions = ('cogs.owner', 'cogs.error_handler', 'cogs.tools')
+extensions = (
+    'cogs.owner',
+    'cogs.error_handler',
+    'cogs.admin',
+    'cogs.tools',
+    'cogs.starboard',
+    'cogs.poofboard',
+    'cogs.summary',
+    'cogs.auto',
+    'cogs.minecraft',
+    'cogs.rubber',
+    'cogs.public')
+# extensions = ('cogs.owner', 'cogs.error_handler', 'cogs.public')
 
-# Intents initialisation
-intents = discord.Intents.default()
-intents.typing = False
-intents.presences = False
-intents.members = True
+class NOMUtils(commands.Bot):
+    def __init__(self):
+        allowed_mentions = discord.AllowedMentions(everyone=False) #allowed_mentions makes it so not able to @everyone ever (globally)
+        # Intents initialisation
+        intents = discord.Intents.default()
+        intents.typing = False
+        intents.presences = False
+        intents.members = True
+        super().__init__(
+            command_prefix=["?"],
+            # command_prefix=["e!"],
+            description='NOM#7666\'s Utility bot',
+            owner_id=421362214558105611,
+            allowed_mentions=discord.AllowedMentions(everyone=False),
+            case_insensitive=True,
+            help_command=None,
+            intents=intents
+        )
 
-bot = commands.Bot(
-    command_prefix=["?"],
-    # command_prefix=["e!"],
-    description='NOM#7666\'s Utility bot',
-    owner_id=421362214558105611,
-    allowed_mentions=discord.AllowedMentions(everyone=False),
-    case_insensitive=True,
-    help_command=None,
-    intents=intents
-) #allowed_mentions makes it so not able to @everyone ever (globally)
+        self.config = {
+            'emojis': {
+                'error': '<:error:938203108012605470>'
+            }
+        }
 
+bot = NOMUtils()
 bot.remove_command('help')
 
 @bot.event
@@ -36,6 +56,7 @@ async def on_ready():
     # await sleep(3)
     # await bot.change_presence(status=discord.Status.invisible)
 
+debug_mode = False #wether or not to connect to the db
 token = getenv('BOT_TOKEN')
 db_username = getenv('DB_USERNAME')
 db_pass = getenv('DB_PASS')
@@ -50,12 +71,14 @@ if __name__ == '__main__':
             traceback.print_exc()
     try:
         # NOTE: 127.0.0.1 is the loopback address. If db is running on the same machine as the code, this address will work
-        credentials = {"user": db_username, "password": db_pass, "database": db_name, "host": db_host}
-        bot.pool = bot.loop.run_until_complete(asyncpg.create_pool(**credentials))
+        if not debug_mode:
+            credentials = {"user": db_username, "password": db_pass, "database": db_name, "host": db_host}
+            bot.pool = bot.loop.run_until_complete(asyncpg.create_pool(**credentials))
         bot.loop.run_until_complete(bot.start(token))
     except KeyboardInterrupt:
         # cancel all tasks lingering
-        bot.loop.run_until_complete(bot.pool.close())
+        if not debug_mode:
+            bot.loop.run_until_complete(bot.pool.close())
         bot.loop.run_until_complete(bot.close())
     finally:
         bot.loop.close()
