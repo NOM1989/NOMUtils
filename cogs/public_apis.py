b65c42fd2499b2ec65bd52bc40d7bc3cf96d9204
NOM1989 <70205169+NOM1989@users.noreply.github.com>
2022-07-09 21:50:58 +0200

    add: public_apis initial: bitly

---
+++ cogs/public_apis.py
@@ -0,0 +1,74 @@
+from discord.ext import commands
+from .utils.context import Context
+from datetime import datetime
+from bot import NOMUtils
+from os import getenv
+import discord
+import aiohttp
+import re
+
+class InvalidLink(commands.CommandError):
+    def __str__(self):
+        return 'Invaid bit.ly link'
+
+class APIS(commands.Cog):
+    def __init__(self, bot):
+        self.bot: NOMUtils = bot
+        self.SEARCH_LIMIT: int = 100
+        self.BITLYAPI_KEY: str = getenv('BITLYAPI_KEY')
+
+    async def cog_check(self, ctx):
+        return await self.bot.is_owner(ctx.author)
+    
+    async def cog_command_error(self, ctx: Context, error: commands.CommandError):
+        if isinstance(error, InvalidLink):
+            await ctx.send_error(str(error))
+
+    @commands.command()
+    async def bitsee(self, ctx: Context, bitlink: str = None):
+        """Requests the long url from a bit.ly link,
+        bitlink sourced from either passed argument
+        or searches last SEARCH_LIMIT messages in channel for link"""
+
+        bitlink_REGEX = r'(https?:\/\/)?((bit\.ly)\S*)\b'
+        
+        if bitlink == None:
+            # Search channel for link
+            async for message in ctx.channel.history(limit=self.SEARCH_LIMIT):
+                bitlink_search = re.search(bitlink_REGEX, message.content)
+                if bitlink_search:
+                    break
+        else:
+            # Check it is a bit.ly link
+            bitlink_search = re.search(bitlink_REGEX, bitlink)
+            
+        # print('here', bitlink_search)
+
+        if bitlink_search:
+            bitlink = bitlink_search.group(2)
+            async with aiohttp.ClientSession() as session:
+                headers = {
+                    'Authorization': f'Bearer {self.BITLYAPI_KEY}',
+                    'Content-Type': 'application/json',
+                }
+                data = f'{{ "bitlink_id": "{bitlink}" }}'
+                async with session.post('https://api-ssl.bitly.com/v4/expand', data=data, headers=headers) as response:
+                    # print('RESPONDED!')
+                    if response.ok:
+                        result = await response.json()
+                        # print(datetime.fromtimestamp(result['created_at']))
+                        date_time = datetime.fromisoformat(result['created_at'][:-2] + ':' + result['created_at'][-2:])
+                        embed = discord.Embed(title=f'Lookup for: `{bitlink}`', description=result['long_url'], timestamp=date_time, color=discord.Colour.green())
+                        embed.set_footer(text='Link created at')
+                        # print('created embed')
+                        return await ctx.send(embed=embed)
+
+                    # When a bitly request does not return the 'ok' status
+                    if ctx.args[2] != None:
+                        raise InvalidLink()
+
+        # print('Here')
+        return await ctx.message.add_reaction('🚫')
+
+def setup(bot):
+    bot.add_cog(APIS(bot))
